39edf8d261028b334034a6aa8cdb4f72285b091f
Erik Demchak <demcsakerik@gmail.com>
2024-05-27 13:41:47 +0200

    add: GraphQL UI

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const { createHandler } = require("graphql-http");
+const { createHandler } = require("graphql-http/lib/use/express");
 const schema = require("./schemas/schema");
+const cors = require("cors");
+const { ruruHTML } = require("ruru/server");
+const bodyParser = require("body-parser");
+
+const url = `http://localhost:4000`;
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.all(
-  "/graphql",
-  createHandler({
-    schema,
-  })
-);
+app.all("/graphql", createHandler({ schema }));
 
-app.listen(4000, () => console.log("Listening on http://localhost:4000"));
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
+});
+
+app.listen(4000, () => console.log(`Listening on ${url}`));
--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -1,40 +1,45 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// Dummy data
+let books = [
+  { name: "Test book 1", genre: "Test genre 1", id: "1" },
+  { name: "Test book 2", genre: "Test genre 2", id: "2" },
+  { name: "Test book 3", genre: "Test genre 3", id: "3" },
+];
+
 const BookType = new GraphQLObjectType({
-  name: "book",
+  name: "Book",
   fields: () => ({
-    id: {
-      type: GraphQLString,
-    },
-    name: {
-      type: GraphQLString,
-    },
-    genre: {
-      type: GraphQLString,
-    },
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
   }),
 });
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
-  fields: () => ({
+  fields: {
     book: {
-      type: BookType,
-      args: {
-        id: {
-          type: GraphQLString,
-        },
-      },
+      type: [],
+      args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        console.log("Book id is: ", args.id);
-        // code to get data from DB or any other source
+        console.log("here");
+        return _.find(books, { id: args.id });
       },
     },
-  }),
+    books: {
+      type: BookType,
+      resolve(parent) {
+        return books;
+      },
+    },
+  },
 });
 
+// Export the schema object directly
 module.exports = new GraphQLSchema({
   query: RootQuery,
 });
